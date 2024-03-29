c70caebee70e3573c97d5491de06ba6ee684699c
Priyansh Nihalani <138807092+priyanshnihalani@users.noreply.github.com>
2024-03-29 06:04:21 -0700

    In_Depth_Javascript

---
+++ Javascript_Basics/P20.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Extra String Items</title>
+</head>
+
+<body>
+  
+  <script>
+    let a = 10;
+    console.log(a.toString(2) + '<br>')
+    console.log(a.toString(8) + '<br>')
+    console.log(a.toString(16) + '<br>')
+  </script>
+</body>
+
+</html>
